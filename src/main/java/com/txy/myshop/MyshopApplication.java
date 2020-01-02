4f89d424b12aa21ee9a2a8b43354a42a66b27db4
Crecendow <1158686128@qq.com>
2020-01-02 17:34:45 +0800

    提交了tomcat部署的版本

--- src/main/java/com/txy/myshop/MyshopApplication.java
+++ src/main/java/com/txy/myshop/MyshopApplication.java
@@ -2,12 +2,16 @@ package com.txy.myshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyshopApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run( MyshopApplication.class, args );
+public class MyshopApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //注意这里的参数要指向原先用main方法执行的Application启动类
+        return builder.sources(MyshopApplication.class);
     }
-
 }
+
+
